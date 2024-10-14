6a6d7b0164dd0057b3ae19381d20b00c8422e928
Thiago Canato de Azevedo <atomoiadev@gmail.com>
2024-10-14 11:16:20 -0300

    pagina que unifica sistemas

---
+++ paginaUnificaSistemas.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Escolha qual sistema deseja acessar</h1>
+
+    <a href="sistema_oficina/"> Oficina </a>
+    <br>
+    <a href="sistema_orgao_publico/"> Órgão Público </a>
+</body>
+</html>
