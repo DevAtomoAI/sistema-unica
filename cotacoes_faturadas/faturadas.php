2b9590800ab8a321ba376c242fb52de0688dbe42
Thiago Canato de Azevedo <atomoiadev@gmail.com>
2024-11-05 14:48:10 -0300

    alteracoes dia 05/10

--- cotacoes_faturadas/faturadas.php
+++ cotacoes_faturadas/faturadas.php
@@ -16,7 +16,7 @@ $idOrgaoPublicoLogado = $_SESSION['idOrgaoPublico'];
 $nomeUsuario = $_SESSION["nameLoggedUser"];
 $idVeiculosInclusosOrgaoPublico = $_SESSION["idVeiculosInclusosOrgaoPublico"];
 
-$selectTableFaturadas = "SELECT * FROM infos_veiculos_inclusos WHERE opcao_aprovada_reprovada_oficina='Faturada Oficina'";
+$selectTableFaturadas = "SELECT * FROM infos_veiculos_inclusos WHERE orcamento_aprovada_reprovada_oficina='Faturada Oficina'";
 
 if (isset($_SESSION['filtrosPesquisaFaturadas']) && !empty($_SESSION['filtrosPesquisaFaturadas'])) {
     $selectTableFaturadas = $_SESSION['filtrosPesquisaFaturadas'];
@@ -25,25 +25,7 @@ if (isset($_SESSION['filtrosPesquisaFaturadas']) && !empty($_SESSION['filtrosPes
 $_SESSION['filtrosPesquisaFaturadas'] = null;
 
 $execConnectionFaturadas = $conexao->query($selectTableFaturadas);
-$numLinhasAprovadas = $execConnectionFaturadas->num_rows;
-
-if ($idVeiculosInclusosOrgaoPublico) {
-    $selectTable2 = "SELECT * FROM infos_veiculos_aprovados_oficina 
-                WHERE id_veiculo_incluso_orgao_publico = $idVeiculosInclusosOrgaoPublico AND id_orgao_publico='$idOrgaoPublicoLogado' AND orcamento_aprovado_reprovado!='Aprovada'
-                AND orcamento_aprovado_reprovado!='Cancelada'";
-    $numLinhasTotal2 = $conexao->query($selectTable2)->num_rows;
-
-    $selectTable3 = "SELECT * FROM infos_veiculos_aprovados_oficina 
-                     WHERE orcamento_aprovado_reprovado = '' AND
-                     id_veiculo_incluso_orgao_publico = $idVeiculosInclusosOrgaoPublico 
-                     AND id_orgao_publico = '$idOrgaoPublicoLogado'";
-    $numLinhasTotal3 = $conexao->query($selectTable3)->num_rows;
-
-    $selectTable4 = "SELECT veiculo FROM infos_veiculos_inclusos 
-                     WHERE id_orgao_publico = '$idOrgaoPublicoLogado' 
-                     AND id_infos_veiculos_inclusos = '$idVeiculosInclusosOrgaoPublico'";
-    $nomeVeiculo = $conexao->query($selectTable4)->fetch_assoc()['veiculo'] ?? '';
-}
+$numLinhasAprovadas = $execConnectionFaturadas->num_rows;   
 
 ?>
 
@@ -117,7 +99,7 @@ if ($idVeiculosInclusosOrgaoPublico) {
                 <select name="centro-custo">
                     <option value="">Centro de Custo</option>
                     <?php
-                    $selectTableOrgaosSolicitantes = "SELECT * FROM infos_veiculos_inclusos WHERE opcao_aprovada_reprovada_oficina='Faturado Oficina' ORDER BY id_infos_veiculos_inclusos ASC";
+                    $selectTableOrgaosSolicitantes = "SELECT * FROM infos_veiculos_inclusos WHERE orcamento_aprovada_reprovada_oficina='Faturada Oficina' ORDER BY id_infos_veiculos_inclusos ASC";
                     $execConnectionOrgaoSolicitante = $conexao->query($selectTableOrgaosSolicitantes);
 
                     while ($orgaoSolicitantes = mysqli_fetch_assoc($execConnectionOrgaoSolicitante)) {
@@ -147,7 +129,7 @@ if ($idVeiculosInclusosOrgaoPublico) {
                         <th>Oficina</th>
                         <th>Fatura peças</th>
                         <th>Fatura serviços</th>
-                        <!-- <th>Opção</th> -->
+                        <th>Opção</th>
                     </tr>
                 </thead>
                 <tbody id="cotacoes-body">
@@ -156,9 +138,7 @@ if ($idVeiculosInclusosOrgaoPublico) {
 
                         while ($userData = mysqli_fetch_assoc($execConnectionFaturadas)) {
                             $idVeiculoIncluso = $userData['id_infos_veiculos_inclusos'];
-
-                            $selectTableFaturadas2 = "SELECT * FROM infos_veiculos_aprovados_oficina WHERE id_veiculo_incluso_orgao_publico='$idVeiculoIncluso'";
-
+                            $selectTableFaturadas2 = "SELECT * FROM infos_veiculos_inclusos WHERE id_infos_veiculos_inclusos='$idVeiculoIncluso'";
                             $execConnectionFaturadas2 = $conexao->query($selectTableFaturadas2);
 
                             while($userData2 = mysqli_fetch_assoc($execConnectionFaturadas2)){
@@ -168,6 +148,7 @@ if ($idVeiculosInclusosOrgaoPublico) {
                                 echo "<td class='resultadosTabela'>" . $userData2['nome_oficina_aprovado'] . "</td>";
                                 echo "<td class='resultadosTabela'><a href='faturasPecas.php?id=" . $userData2['id_veiculo_incluso_orgao_publico'] . "'> Fatura peças</a></td>";
                                 echo "<td class='resultadosTabela'> <a href='faturasServicos.php?id=" . $userData2['id_veiculo_incluso_orgao_publico'] . "'> Fatura serviço </a></td>";
+                                echo "<td><button>Faturar</button></td>";
                                 echo "</tr>";
                             }
 
