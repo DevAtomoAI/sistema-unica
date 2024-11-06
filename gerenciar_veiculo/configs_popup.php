c721dbbea7e315f7d7878e3e76edbf873a8fcae6
Thiago Canato de Azevedo <atomoiadev@gmail.com>
2024-11-06 11:30:54 -0300

    alteracoes dia 06/11

--- gerenciar_veiculo/configs_popup.php
+++ gerenciar_veiculo/configs_popup.php
@@ -4,6 +4,11 @@
 include_once("../database/config.php");
 session_start();
 
+ini_set('display_errors', 1);  // Exibe os erros no navegador
+ini_set('display_startup_errors', 1);  // Exibe erros durante a inicialização
+error_reporting(E_ALL);  // Relata todos os tipos de erro
+
+
 // Variáveis gerais
 $idVeiculoGerenciado = $_SESSION['idVeiculoGerenciar'];
 $nomeOficina = $_SESSION['nomeOficina'];
@@ -26,12 +31,21 @@ function adicionaValoresCotacaoOficina($connectionDB, $idVeiculoGerenciado, $nom
     $selectInfosServicosXML = "SELECT * FROM infos_cotacao_orgao WHERE id_veiculo_incluso_orgao_publico = '$idVeiculoGerenciado' AND id_orgao_publico = '$idOrgaoPublico' AND quantidade_pecas = 0";
     $result3 = $connectionDB->query($selectInfosServicosXML);
 
+    $selectIDOficina = "SELECT id_usuarios_oficina FROM usuarios_oficina WHERE nome_oficina = '$nomeOficina'";
+    $result4 = $connectionDB->query($selectIDOficina);
+    $resultados4 = $result4->fetch_assoc();
+    $idOficina = $resultados4['id_usuarios_oficina'];
+
+    $result3 = $connectionDB->query($selectInfosServicosXML);
+
     $valores = []; // Inicializa o array para armazenar os valores
-    $contador = 1;
-    $contador2 = 1;
+    $contador = 0;
+    $contador2 = 0;
 
     while ($resultados2 = $result2->fetch_assoc()) {
         // Coleta dados de peças
+        $contador++; // Incrementa o contador
+
         $idInfosCotacaoOrgao = $resultados2['id_infos_cotacao_orgao'];
         $marcaPecas = $_POST['marcaPecas' . $contador];
         $valorUNPecas = (float)$_POST['valorUNPecas' . $contador];
@@ -41,18 +55,19 @@ function adicionaValoresCotacaoOficina($connectionDB, $idVeiculoGerenciado, $nom
             break;
         }
         // Adiciona os valores ao array sem valorOrcadoServicos
-        $valores[] = "('$idOrgaoPublico', '$idVeiculoGerenciado', '$idInfosCotacaoOrgao', '$nomeOficina', '$marcaPecas', '$valorUNPecas', NULL, '$prazoEntrega', true)";
+        $valores[] = "('$idOrgaoPublico', '$idVeiculoGerenciado', '$idInfosCotacaoOrgao', '$idOficina', '$nomeOficina', '$marcaPecas', '$valorUNPecas', NULL, '$prazoEntrega', true)";
     
-        $contador++; // Incrementa o contador
     }
 
     while ($resultados3 = $result3->fetch_assoc()) {
         // Coleta dados de peças
+        $contador2++; // Incrementa o contador
+
+        $idInfosCotacaoOrgao = $resultados3['id_infos_cotacao_orgao'];
         $valorOrcadoServicos = (float)$_POST['valorOrcadoServico' . $contador2]; 
         // Adiciona os valores ao array sem valorOrcadoServicos
-        $valores[] = "('$idOrgaoPublico', '$idVeiculoGerenciado', '$idInfosCotacaoOrgao', '$nomeOficina', '$marcaPecas', '$valorUNPecas', '$valorOrcadoServicos', '$prazoEntrega', true)";
+        $valores[] = "('$idOrgaoPublico', '$idVeiculoGerenciado', '$idInfosCotacaoOrgao', '$idOficina', '$nomeOficina', '$marcaPecas', '$valorUNPecas', '$valorOrcadoServicos', '$prazoEntrega', true)";
     
-        $contador2++; // Incrementa o contador
     }
 
 
@@ -62,11 +77,13 @@ function adicionaValoresCotacaoOficina($connectionDB, $idVeiculoGerenciado, $nom
     if (!empty($valores)) {
         $sqlInsert = "
         INSERT INTO orcamentos_oficinas (
-            id_orgao_publico, id_veiculo_gerenciado, id_infos_cotacao_orgao, nome_oficina, marca_pecas, valor_un_pecas, valor_orcado_servicos, dias_execucao, orcado_oficina
+            id_orgao_publico, id_veiculo_gerenciado, id_infos_cotacao_orgao, id_oficina, nome_oficina, marca_pecas, valor_un_pecas, valor_orcado_servicos, dias_execucao, orcado_oficina
         ) VALUES " . implode(", ", $valores);
     
         // Executa a consulta
-        $connectionDB->query($sqlInsert);
+        if($connectionDB->query($sqlInsert)){
+            echo "query executado";
+        }
     }
     
 }
